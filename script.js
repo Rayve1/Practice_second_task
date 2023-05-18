d44ba1209264ca019bbef9d10a326ca5327ebf26
Rayve1 <rayve3432@gmail.com>
2023-05-18 12:08:24 +0400

    Added text_array

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
 let random_text_button = document.querySelector(".random_text_button")
 let clean_button = document.querySelector(".clean_button")
 let display = document.querySelector(".display")
+let text_array = ["Hello World", "How are You", "if life gives you lemons make lemonade", "Scientia potentia est", "Per aspera ad astra!", "Forever young", "Let it be"]
